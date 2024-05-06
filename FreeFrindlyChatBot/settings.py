8f08eff47815afc12659640853bc4459ee669270
NagiPragalathan <nagipragalathan@gmail.com>
2024-05-06 10:14:36 +0530

    chatbot complete

--- FreeFrindlyChatBot/settings.py
+++ FreeFrindlyChatBot/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = "django-insecure-eta_xo#3&02j7y#i9_j9d_2cjm&^3ail)@*ke@j)o^nku^q$xx
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
-
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app', '.now.sh','*']
 
 # Application definition
 
@@ -115,7 +114,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
+import os
+
+STATIC_URL = 'static/'
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_URL = '/media/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
